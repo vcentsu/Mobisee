0eba600f93ba3d64fc97a16cae14f82085650652
vcentsu <vincenttius.sutanto@gmail.com>
2022-06-13 00:26:22 +0700

    Initial Commit

---
+++ Mobisee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mobisee
+//
+//  Created by Vincentius Sutanto on 13/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
