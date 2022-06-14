7374ad572b2c404b4f6eaafa773beb6c38f855c1
Anak Agung Gede Agung Davin <anakagung02@mail.ugm.ac.id>
2022-06-14 15:28:42 +0700

    Added GMaps for trial. Please install cocoapods also setup the podfile.
    ############################## Pod File Configuration
    # Uncomment the next line to define a global platform for your project
    # platform :ios, '15.4'
    
    target 'Mobisee' do
      # Comment the next line if you don't want to use dynamic frameworks
      use_frameworks!
      pod 'GoogleMaps', '6.2.1'
      pod 'GooglePlaces', '6.2.1'
    
      # Pods for Mobisee
    
    end

--- Mobisee/AppDelegate.swift
+++ Mobisee/AppDelegate.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        GMSServices.provideAPIKey("AIzaSyD18dw7joeRivkaDvTm5IzsLTxT-BjJo68")
+        GMSPlacesClient.provideAPIKey("AIzaSyCtqBUAWmad-1yoHww05Z6XS7jKfZZWdXo")
+        
         return true
     }
 
--- Mobisee/Controller/ViewController.swift
+++ Mobisee/Controller/ViewController.swift
@@ -6,12 +6,42 @@
 //
 
 import UIKit
+import GoogleMaps
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
         // Do any additional setup after loading the view.
+        let coordinate = location.coordinate
+        let camera = GMSCameraPosition.camera(withLatitude: coordinate.latitude, longitude: coordinate.longitude, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: view.frame, camera: camera)
+        view.addSubview(mapView)
+
+       // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = coordinate
+        marker.title = "Sydneyaaa"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+        print("License: \n\n\(GMSServices.openSourceLicenseInfo())")
+        
     }
 
 
