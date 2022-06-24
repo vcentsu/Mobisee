f2f25a1d4e6e4a2d7efd6d017a99b7890a496ae6
vcentsu <vincenttius.sutanto@gmail.com>
2022-06-24 13:01:17 +0700

    add asset & update ui onboard

--- Mobisee/Controller/OnBoardViewController.swift
+++ Mobisee/Controller/OnBoardViewController.swift
@@ -7,23 +7,39 @@
 
 import UIKit
 
-class OnBoardViewController: UIViewController {
+class OnBoardViewController: UIViewController , UICollectionViewDataSource , UICollectionViewDelegate{
 
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var img = ["Card1", "Card2", "Card3"]
+    var btn = [true, true, false]
+    var activePage: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        pageControl.numberOfPages = img.count
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return img.count
     }
-    */
-
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = (collectionView.dequeueReusableCell(withReuseIdentifier: "ItemCellID", for: indexPath) as? ItemCell)!
+        
+        cell.image.image = UIImage(named: img[indexPath.row])
+        cell.button.isHidden = btn[indexPath.row]
+        return cell
+    }
+    
+    @IBAction func pressSkipBtn(_ sender: Any) {
+        activePage = 1
+        collectionView.scrollToItem(at: IndexPath(item: 2, section: 1), at: .centeredHorizontally, animated: true)
+    }
 }
--- Mobisee/Controller/StartViewController.swift
+++ Mobisee/Controller/StartViewController.swift
@@ -13,6 +13,7 @@ class StartViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        navigationItem.backBarButtonItem?.isEnabled = false
     }
     
     @IBAction func pressPlanBtn(_ sender: Any) {
